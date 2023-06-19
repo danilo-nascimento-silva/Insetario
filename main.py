27f33399f60b6db4887697533716a30da2bba468
Danilo Nascimento <112949289+danilo-nascimento-silva@users.noreply.github.com>
2023-06-19 00:02:43 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2 as cv 
+
+camera = cv.VideoCapture(0)
+rodando = True
+
+while rodando:
+
+    status, frame = camera.read()
+
+    if not status or cv.waitKey(1) & 0xff == ord('q'):
+        rodando = False
+
+    frame = frame[::-1]
+    cv.imshow("Camera", frame)
