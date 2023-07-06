32b4d207b060503ce719d3cbd6d0428979a9ca98
Danilo Nascimento <112949289+danilo-nascimento-silva@users.noreply.github.com>
2023-07-06 00:12:21 -0300

    a
    a

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 import requests
+import json
 
 link = 'https://insetario-uesc-default-rtdb.firebaseio.com/'
 
-def getDHT():
 
-    r = requests.get(f'{link}.json')
-    h, t = r.json()
-    return h, t
+requisicao = requests.get(f'{link}/.json')
+dic_requisicao = requisicao.json()
 
-humidity, temperature = getDHT()
-print(humidity, temperature)
+temperature = dic_requisicao['temperatura']
+humidity = dic_requisicao['umidade']
+datetime = dic_requisicao['datetime']
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperatura", temperature, "anterior")
-col3.metric("Umidade", humidity, "Anterior")
+col1, col2  = st.columns(2)
+col1.metric("Temperatura", temperature)
+col2.metric("Umidade", humidity)
 
